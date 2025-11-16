0ef40209c2d718af4bfe6dcefe11ff7b091abaa5
lindaaaaen <hanxien24@gmail.com>
2025-11-16 17:12:58 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,23 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Represents a performance of a play, including which play it is
+ * and the size of the audience.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
-    public Performance(String playID, int audience) {
-        this.playID = playID;
-        this.audience = audience;
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
     }
 }
+
+// public Performance(String playID, int audience) {
+// this.playID = playID;
+// this.audience = audience;
